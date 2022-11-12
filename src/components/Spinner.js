89cf9f34b3a8609a13158adcc29631edc5d349c3
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-11 21:16:45 -0300

    Spinner component

---
+++ src/components/Spinner.js
@@ -0,0 +1,14 @@
+const Spinner = ({ message }) => {
+  return (
+    <>
+      <div className="ui active dimmer">
+        <div className="ui big text loader">{message}</div>
+      </div>
+    </>
+  );
+};
+
+Spinner.defaultProps = {
+  message: "Loading...",
+};
+export default Spinner;
