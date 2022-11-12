8da42702e075ce4aea71738001fb1083ed3528ae
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-11 21:16:31 -0300

    SeasonDisplay component & style

--- src/components/SeasonDisplay.js
+++ src/components/SeasonDisplay.js
@@ -1,5 +1,33 @@
-const SeasonDisplay = () => {
-  return <div>SeasonDisplay</div>;
+import "./SeasonDisplay.css";
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Burr, it's chilly!",
+    iconName: "snowflake",
+  },
+};
+const SeasonDisplay = ({ lat }) => {
+  const season = getSeason(lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`${iconName} icon massive icon-left`}></i>
+      <h1>{text}</h1>
+      <i className={`${iconName} icon massive icon-right`}></i>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
