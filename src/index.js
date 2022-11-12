9594ec240a3c0f8ae8ae12101d3c0849ce2cdd51
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-11 21:17:03 -0300

    App component | index

--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import SeasonDisplay from "./components/SeasonDisplay";
+import Spinner from "./components/Spinner";
+import "./style/App.css";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: "" };
 
-    this.state = { lat: null, errorMessage: "" };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
+      (position) => this.setState({ lat: position.coords.latitude }),
       (err) => this.setState({ errorMessage: err.message })
     );
   }
 
-  render() {
+  renderContent() {
+    console.log(this.state);
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <div>Loading...</div>;
+    return <Spinner message={"Please, accept location request."} />;
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 const root = ReactDOM.createRoot(document.querySelector("#root"));
